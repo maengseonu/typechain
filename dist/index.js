c6e323e9e2f74aff070071358daf7f101c366032
maengseonu <aodtjsdn123@gmail.com>
2022-01-04 15:03:36 +0900

    class

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const lynn = new Human("Lynn", 18, "female");
+const sayHi = (person) => {
+    return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+console.log(sayHi(lynn));
+//# sourceMappingURL=index.js.map
